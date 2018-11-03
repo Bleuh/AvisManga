2777d514da296b7bf6d005d8f09b2a0113e72f5a
babariviere <babathriviere@gmail.com>
2018-11-03 12:21:54 +0100

    [db] add login request

--- lib/data/db.dart
+++ lib/data/db.dart
@@ -1,19 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:avis_manga/models/manga.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 const String mangaCollection = "Manga";
 
 class Database {
   static final Database _instance = new Database._internal();
 
-  static Firestore db = Firestore.instance;
+  static FirebaseAuth _auth = FirebaseAuth.instance;
+  static Firestore _db = Firestore.instance;
+  static GoogleSignIn _googleSignIn = GoogleSignIn();
 
   static get instance => _instance;
 
   Database._internal();
 
+  // Authentification
+
+  Future<FirebaseUser> googleSignIn() async {
+    return _googleSignIn.signIn().then((user) {
+      return user.authentication.then((auth) {
+        return _auth.signInWithGoogle(
+          accessToken: auth.accessToken,
+          idToken: auth.idToken,
+        );
+      });
+    });
+  }
+
+  Future<FirebaseUser> emailSignIn(String email, String password) async {
+    return _auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  // Manga
+
   Future<List<MangaMetadata>> listMangas() async {
-    return db.collection(mangaCollection).getDocuments().then((query) {
+    return _db.collection(mangaCollection).getDocuments().then((query) {
       return query.documents.map((doc) {
         return MangaMetadata.fromMap(doc.data);
       }).toList();
@@ -21,7 +44,7 @@ class Database {
   }
 
   Future<MangaMetadata> getManga(String title) async {
-    return db.collection(mangaCollection).document(title).get().then((doc) {
+    return _db.collection(mangaCollection).document(title).get().then((doc) {
       if (!doc.exists) {
         return Future.error("missing data");
       }
@@ -30,13 +53,13 @@ class Database {
   }
 
   Future<void> insertManga(MangaMetadata manga) async {
-    return db
+    return _db
         .collection(mangaCollection)
         .document(manga.title)
         .setData(manga.toMap());
   }
 
   Future<void> deleteManga(String title) async {
-    return db.collection(mangaCollection).document(title).delete();
+    return _db.collection(mangaCollection).document(title).delete();
   }
 }
