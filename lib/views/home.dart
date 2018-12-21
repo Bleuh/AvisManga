56717d35993026b039b18f05ccc193776bfb789c
babariviere <babathriviere@gmail.com>
2018-12-21 14:14:55 +0100

    [home] disable pop back

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -37,33 +37,36 @@ class _HomePageState extends State<HomePage> implements AuthListener {
     var widgets = this.widget.data.values.map((meta) {
       return MangaCard(meta);
     }).toList();
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the HomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: () {
-              Auth.instance.then((a) => a.doLogout());
-            },
-          )
-        ],
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: widgets,
+    return WillPopScope(
+      onWillPop: () => Future.value(false),
+      child: new Scaffold(
+        appBar: new AppBar(
+          // Here we take the value from the HomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {
+                Auth.instance.then((a) => a.doLogout());
+              },
+            )
+          ],
         ),
+        body: new Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widgets,
+          ),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: () => print("Pressed"),
+          tooltip: 'Search',
+          child: new Icon(Icons.search),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () => print("Pressed"),
-        tooltip: 'Search',
-        child: new Icon(Icons.search),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
